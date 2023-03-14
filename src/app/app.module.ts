0c082c1d0f76445204068b9e19cff806d81805d4
carlosedubc <carlosedubc@gmail.com>
2023-03-14 20:52:15 -0300

    projetoSem09Mod01 - Exercício02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
