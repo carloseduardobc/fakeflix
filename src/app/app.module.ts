482d1920b1859dbe8d76868ee34523be566ad816
carlosedubc <carlosedubc@gmail.com>
2023-03-15 00:09:53 -0300

    Criação da Página Series (EX03_SEM09_MOD01)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SeriesComponent } from './series/series.component';
+import { CardComponent } from './components/card/card.component';
+import { SeriesComponent } from "./pages/series/series.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CardComponent,
     SeriesComponent
+
   ],
   imports: [
     BrowserModule,
